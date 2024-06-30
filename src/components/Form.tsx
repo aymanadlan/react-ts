940ea1598076b9715b1c6d43d6f629cb1c68d1a0
Ayman Adlan <aymanadlan@hotmail.com>
2024-06-30 18:44:57 +0400

    useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,15 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 type Props = {};
 
 const Form = (props: Props) => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-
-    if (nameRef.current !== null) {
-      person.name = nameRef.current.value;
-    }
-
-    if (ageRef.current !== null) {
-      person.age = parseInt(ageRef.current.value);
-    }
 
     console.log(person);
   };
@@ -27,13 +19,29 @@ const Form = (props: Props) => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          value={person.name}
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          value={person.age}
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
