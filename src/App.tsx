bad0f0f288b7cf2cd5bf15e6006cd6f19d32b8a7
Ayman Adlan <aymanadlan@hotmail.com>
2024-07-02 15:54:09 +0400

    Finalize form

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Utilities", "Groceries", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 function app() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -22,7 +21,11 @@ function app() {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
 
       <div className="mb-3">
